638069527806e4b0c20b316cebba6feb4ea5b1bb
Loller <loller237@gmail.com>
2022-12-23 23:14:58 +0400

    before javascript

---
+++ script.js
@@ -0,0 +1,42 @@
+function active(event)  {
+    event.target.style.color = "hsl(183, 100%, 15%)";
+    event.target.style.background = "hsl(172, 67%, 45%)";
+    event.target.setAttribute("state", "active");
+    document.getElementById("custom-percentage").setAttribute("state", "inactive");
+    let bill= parseInt(document.getElementById("bill-amount").value);
+    let tiprate = parseInt(event.target.value)/100 ;
+    let numberOfPeople =  parseInt(document.getElementById("people-amount").value);
+    let tipperperson = bill*tiprate/numberOfPeople;
+    let total = (bill*tiprate+bill)/numberOfPeople;
+    console.log(Math.round(100*tipperperson)/100);
+    document.getElementById("tipperperson").innerText = "$"+Math.round(100*tipperperson)/100;
+    document.getElementById("totalperperson").innerText = "$"+Math.round(100*total)/100;
+    let parent = event.target.parentNode;
+    for (let i= 0; i<parent.children.length; i++){
+        let child = parent.children[i];
+        if (child.getAttribute("class")=="default-value" && child.value!=event.target.value) {
+            child.style.color = "hsl(0, 0%, 100%)";
+            child.style.background = "hsl(183, 100%, 15%)";
+            child.style.hover
+            child.setAttribute("state", "inactive");
+    }
+}
+}
+
+function active2 (event) {
+    event.target.style.color = "hsl(183, 100%, 15%)";
+    event.target.style.background = "hsl(172, 67%, 45%)";
+    event.target.setAttribute("state", "active");
+    let bill=document.getElementById("bill-amount").value;
+    let tiprate = event.target.value;
+    let parent = event.target.parentNode;
+    for (let i= 0; i<parent.children.length; i++){
+        let child = parent.children[i];
+        if (child.getAttribute("class")=="default-value" && child.value!=event.target.value) {
+            child.style.color = "hsl(0, 0%, 100%)";
+            child.style.background = "hsl(183, 100%, 15%)";
+            child.setAttribute("state", "inactive");
+    }
+}
+}
+
